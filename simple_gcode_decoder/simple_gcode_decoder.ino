c06f32cf2b6091f369b2c24a79840c289d1fa849
monclubelec <support@mon-club-elec.fr>
2015-06-19 12:00:34 +0200

    home axis message

--- simple_gcode_decoder/simple_gcode_decoder.ino
+++ simple_gcode_decoder/simple_gcode_decoder.ino
@@ -351,6 +351,7 @@ void help() {
 	Serial.println(F("G00 [X(steps)] [Y(steps)] [F(feedrate)]; - linear move"));
 	Serial.println(F("G01 [X(steps)] [Y(steps)] [F(feedrate)]; - linear move"));
 	Serial.println(F("G04 P[seconds]; - delay"));
+	Serial.println(F("G28 [X] [Y] [Z]; - home axis"));
 	Serial.println(F("G90; - absolute mode"));
 	Serial.println(F("G91; - relative mode"));
 	Serial.println(F("G92 [X(steps)] [Y(steps)]; - change logical position"));
